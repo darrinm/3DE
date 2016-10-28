25d003022e5321de03a32862e064721365ae3c87
Darrin Massena <darrin@massena.com>
2016-10-28 14:59:05 -0700

    Disable browser caching of published projects

--- editor/js/TDE.js
+++ editor/js/TDE.js
@@ -43,28 +43,29 @@ TDE.publish = function( projectId, title, files ) {
 // TODO: don't allow public write access!
 TDE.upload = function( bucket, object, data ) {
 
+	var mimeTypes = {
+		html: 'text/html',
+		json: 'application/json',
+		js: 'text/javascript',
+		png: 'image/png',
+		jpg: 'image/jpeg',
+		jpeg: 'image/jpeg'
+	}
+	var contentType = mimeTypes[ object.split( '.' ).pop().toLowerCase() ] || 'text/plain';
+
+	// Get resumable upload session URI.
+
 	return new Promise( function( resolve, reject ) {
 
 		var xhr = new XMLHttpRequest();
-		xhr.open( 'POST', 'https://www.googleapis.com/upload/storage/v1/b/' + bucket +
-		'/o?uploadType=media&predefinedAcl=publicRead&name=' + object, true );
-
-		var mimeTypes = {
-			html: 'text/html',
-			json: 'application/json',
-			js: 'text/javascript',
-			png: 'image/png',
-			jpg: 'image/jpeg',
-			jpeg: 'image/jpeg'
-		}
-		var contentType = mimeTypes[ object.split( '.' ).pop().toLowerCase() ] || 'text/plain';
-		xhr.setRequestHeader( 'Content-Type', contentType );
+		xhr.open( 'POST', 'https://www.googleapis.com/upload/storage/v1/b/' + bucket + '/o?uploadType=resumable&predefinedAcl=publicRead', true );
+		xhr.setRequestHeader( 'Content-Type', 'application/json' );
 
 		xhr.onload = function( event ) {
 
 			if ( this.status === 200 || this.status === 0 ) {
 
-				resolve( this.response );
+				resolve( xhr.getResponseHeader( 'Location' ) );
 
 			} else {
 
@@ -74,14 +75,17 @@ TDE.upload = function( bucket, object, data ) {
 
 		}
 
-		xhr.send( data );
+		// Disable browser caching. Consider inserting version numbers as cache-busting query parameters instead.
+		xhr.send( JSON.stringify( { name: object, 'contentType': contentType, cacheControl: 'private, max-age=0, no-transform' } ) );
 
-	} ); /*.then( function( response ) {
+	// Upload the file to the upload session URI.
+
+	} ).then( function( uploadLocation ) {
+
 		return new Promise( function( resolve, reject ) {
 
 			var xhr = new XMLHttpRequest();
-			xhr.open( 'POST', 'https://www.googleapis.com/storage/v1/b/' + bucket + '/o/' + encodeURIComponent( object ), true );
-			xhr.setRequestHeader( 'Content-Type', 'application/json' );
+			xhr.open( 'PUT', uploadLocation, true );
 
 			xhr.onload = function( event ) {
 
@@ -97,10 +101,12 @@ TDE.upload = function( bucket, object, data ) {
 
 			}
 
-			xhr.send( '{ "acl": [], "cacheControl": "private" }' );
+			xhr.send( data );
 
 		} );
-	} );*/
+
+	} );
 
 }
+
 
