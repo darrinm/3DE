58eb7623b7d29b303fddb944092b7ecbb4861e52
Darrin Massena <darrin@massena.com>
2016-11-03 23:26:12 -0700

    publish: Partial delete project functionality
    generated from commit 5f69eba49ba94c9a869052879155ea1d55586c88

--- exportscene.js
+++
@@ -1,6 +0,0 @@
-//var script=document.createElement('script');
-//script.src='http://localhost:8080/scenexporter.js';
-//script.className='ExportScene';document.head.appendChild(script);%7D)()
-var output = scene.toJSON();
-output = JSON.stringify( output, null, '\t' );
-console.log(output);
